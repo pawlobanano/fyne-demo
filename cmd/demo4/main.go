df2c34f497dce656eac1a5edddb80b3476fd174a
Paweł Banach <1054126+pawlobanano@users.noreply.github.com>
2024-11-11 12:11:44 +0100

    feat: Finish of demo4 - TODO list app

--- cmd/demo4/main.go
+++ cmd/demo4/main.go
@@ -1,12 +1,18 @@
 package main
 
-import "fyne.io/fyne/v2/app"
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+)
 
 func main() {
 	a := app.NewWithID("io.fyne.workshop.todo")
 	w := a.NewWindow("TODO")
 
-	w.SetContent(makeUI())
+	data := a.Preferences().StringListWithFallback("todos",
+		[]string{"Use this TODO list", "Build more Fyne apps"})
 
+	w.SetContent(makeUI(data))
+	w.Resize(fyne.NewSize(180, 240)) // point-size
 	w.ShowAndRun()
 }
--- cmd/demo4/mainUI.go
+++ cmd/demo4/mainUI.go
@@ -1,26 +1,51 @@
 package main
 
 import (
+	"slices"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
-func makeUI() fyne.CanvasObject {
-	list := widget.NewList(
+func makeUI(data []string) fyne.CanvasObject {
+	var list *widget.List
+	list = widget.NewList(
 		func() int {
-			return 5
+			return len(data)
 		},
 		func() fyne.CanvasObject {
 			return widget.NewCheck("An item title", nil)
 		},
 		func(i widget.ListItemID, co fyne.CanvasObject) {
+			ch := co.(*widget.Check)
+			ch.SetText(data[i])
+
+			ch.OnChanged = func(done bool) {
+				if !done {
+					return
+				}
+				ch.SetChecked(false)
+
+				data = slices.Delete(data, i, i+1)
+				list.Refresh()
+				fyne.CurrentApp().Preferences().SetStringList("todos", data)
+			}
 		})
 
 	input := widget.NewEntry()
 	add := widget.NewButtonWithIcon("",
-		theme.ContentAddIcon(), func() {})
+		theme.ContentAddIcon(), func() {
+			if input.Text == "" {
+				return
+			}
+
+			data = append(data, input.Text)
+			list.Refresh()
+			input.SetText("")
+			fyne.CurrentApp().Preferences().SetStringList("todos", data)
+		})
 
 	top := container.NewBorder(nil, nil, nil, add, input)
 	return container.NewBorder(top, nil, nil, nil, list)
